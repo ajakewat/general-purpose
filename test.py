f7d64f488ba3c63dd1f92dfb4fda2e4d8c0f9820
Ajay Kewat <ajakewat@publicisgroupe.net>
2021-05-20 11:46:19 +0530

    - pushed into development

--- test.py
+++ test.py
@@ -9,3 +9,4 @@ Created on Thu May 20 11:13:30 2021
 print("This is my 2nd git py push ")
 print("This is my 3rd git commit ")
 print("This commit will be pushed directly to master")
+print("This commit will goes to developmet branch")
